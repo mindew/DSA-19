93f707d60b8c049b7421a2df5f3d25b84e224be3
mindew <minju.kang@students.olin.edu>
2019-02-22 15:26:36 -0500

    Day2 done

--- sorting/day02/src/FrequencyPrint.java
+++ sorting/day02/src/FrequencyPrint.java
@@ -1,8 +1,71 @@
+import java.util.*;
+
+
 public class FrequencyPrint {
 
     static String frequencyPrint(String s) {
         // TODO
-        return "";
+        String result = "";
+        String delims = "[ ]+";
+        HashMap<String, Integer> dict = new HashMap();
+        String[] tokens = s.split(delims);
+
+        for (int x = 0; x < tokens.length; x++) {
+            if (dict.containsKey(tokens[x])) {
+                int currval = dict.get(tokens[x]);
+                currval++;
+                dict.put(tokens[x], currval);
+            } else {
+                dict.put(tokens[x], 1);
+            }
+        }
+
+
+        for (Object objectName : dict.keySet()) {
+            System.out.println(objectName);
+            System.out.println(dict.get(objectName));
+        }
+        System.out.println("hashmapends");
+
+        HashMap<Integer, String> reverseddict = new HashMap();
+        Set<String> ksdict = dict.keySet();
+
+        for (String kesss: ksdict) {
+            int newKey = dict.get(kesss);
+            System.out.println(newKey);
+            if (reverseddict.containsKey(newKey)) {
+                // if the value of dict's key exists in reverseddict
+                // get the existing string of the key in reverseddict
+                String word = reverseddict.get(newKey);
+                // if newKey is greater than 1
+                // in other words, this word happens more than once
+                for (int i = 0; i < newKey; i++) {
+                    word = word + " " + kesss;
+                }
+
+                reverseddict.put(newKey, word);
+
+            } else {
+                String word = "";
+                for (int i = 0; i < newKey; i++) {
+                    word = word + " " + kesss;
+                }
+                reverseddict.put(newKey, word);
+            }
+        }
+
+        Set<Integer> ks = reverseddict.keySet();
+        int maxelem = Collections.max(ks);
+        for (int x = 1; x <= maxelem; x++) {
+            if (reverseddict.containsKey(x)) {
+                result = result + reverseddict.get(x);
+            }
+        }
+
+
+        System.out.println(result);
+        return result.strip();
+
     }
 
 }
--- sorting/day02/src/LocksAndKeys.java
+++ sorting/day02/src/LocksAndKeys.java
@@ -6,52 +6,59 @@ public class LocksAndKeys {
         A[j] = t;
     }
 
-    public void quickSort(int[] a, int lo, int hi) {
-        if (lo < hi) {
-            int p = partition(a, lo, hi);
-            quickSort(a, lo, p-1);
-            quickSort(a, p+1, hi);
-        }
-    }
-
-    public int partition(int[] array, int lo, int hi) {
-        int p = array[lo];
-        int l = lo+1;
-        for (int x = lo+1; x<=hi; x++) {
-            if (array[x]<p) {
-                swap(array, l, x);
-                l++;
-            }
-        }
-        swap(array, lo, l-1);
-        return l-1;
-    }
 
     static char[][] locksAndKeys(char[] locks, char[] keys) {
         // TODO: Return a 2d char array, where result[0] is the sorted locks, and result[1] is the sorted keys
-
-        for (int x=0; x<locks.length; x++) {
-            char pivot = locks[x];
-            for (int y=0; x<keys.length; y++) {
-                if (keys[y] < pivot) {
-                    swap()
-                }
-
-            }
-        }
-
-
-
-
-
-
+        quickSort(locks, keys, 0, locks.length-1);
+        // start quicksorting from the last element
         char[][] result = new char[2][];
         result[0] = locks;
         result[1] = keys;
+        for (int x = 0; x<result.length; x++)
+            System.out.println(result[x]);
+
         return result;
     }
+
+
+    public static int partition(char[] array, int lo, int hi, char p) {
+        int l = lo ;
+        for (int x = lo; x < hi; x++) {
+
+            // if element is smaller than pivot from other array
+            if (array[x] < p) {
+                // switch the element where the pointer is in the subarray
+                // increment the pointer
+                swap(array, l++, x);
+            }
+            else if(array[x] == p){
+                // if the element is equal to the pivot
+                // switch the last element of the subarray with the element you looked before
+                swap(array, x--, hi);
+            }
+        }
+
+        // lastly, swap pivot with the last element swapped
+        swap(array, l, hi);
+
+        // return the index of lastly swapped element
+        return l;
+    }
+
+    public static void quickSort(char[] locks, char[] keys, int lo, int hi) {
+        if (lo < hi) {
+            // last character of the locks is the "pivot" of keys array
+            int p = partition(locks, lo, hi, keys[hi]);
+
+            // compare the last element (initially), the pivot with all
+            // elements in the subarray of keys
+            partition(keys, lo, hi, locks[p]);
+
+            // quicksort recursively
+            quickSort(locks, keys, lo, p - 1);
+            quickSort(locks, keys, p + 1, hi);
+        }
+    }
+
 }
-
-
-
 
